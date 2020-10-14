c21d51d89e9f9847c4d5e6cd500500e8575f1e12
samkohlleffel <sam.kohlleffel@gmail.com>
2020-10-14 17:23:40 -0500

    trying to fix styling

--- snowflake_estimator/src/Components/CalcResults.js
+++ snowflake_estimator/src/Components/CalcResults.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Container, Row, Col} from "react-bootstrap";
+import {Button, Container} from "react-bootstrap";
 import LineChart from "./LineChart.js"
 import '../main.css';
 
@@ -7,8 +7,8 @@ class Results extends React.Component {
     render() {
         if (this.props.med_calc_results) {
               return <Container fluid id="overlay" className="w-100 h-100">
-                          <div id="overlay_content" className="position-relative col-8 row-8 row-xs-6 col-xs-6 order-sm-1 mx-auto">
-                                <h1 className="row-sm">{this.props.med_calc_results}</h1>
+                          <div id="overlay_content" className="position-relative col-8 col-xs-6 order-sm-1 mx-auto">
+                                <h1 className="row-lg">{this.props.med_calc_results}</h1>
                                 <h4 className="row-sm">Annual Snowflake Credits</h4>
                                 <LineChart className="row-sm"low_calc_results={this.props.low_calc_results} med_calc_results={this.props.med_calc_results} high_calc_results={this.props.high_calc_results}/>
                                 <Button variant="btn btn-primary btn-lg" type="submit" className="overlay_button" onClick={this.props.handler}>Make Another Calculation</Button>
