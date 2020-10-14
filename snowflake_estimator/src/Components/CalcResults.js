41f70b7d68fd2b521bd03f729b11e839c9db307d
samkohlleffel <sam.kohlleffel@gmail.com>
2020-10-14 18:05:50 -0500

    testing react styling

--- snowflake_estimator/src/Components/CalcResults.js
+++ snowflake_estimator/src/Components/CalcResults.js
@@ -7,9 +7,9 @@ class Results extends React.Component {
     render() {
         if (this.props.med_calc_results) {
               return <Container fluid id="overlay" className="w-100 h-100">
-                          <div id="overlay_content" className="position-relative col-8 col-xs-6 order-sm-1 mx-auto">
-                                <h1 className="row-lg">{this.props.med_calc_results}</h1>
-                                <h4 className="row-sm">Annual Snowflake Credits</h4>
+                          <div id="overlay_content" className="position-relative col-lg-8 col-md-4 col-xs-2 order-sm-1 mx-auto">
+                                <h1 className="credits">{this.props.med_calc_results}</h1>
+                                <h4 className="overlay_text">Annual Snowflake Credits</h4>
                                 <LineChart className="row-sm"low_calc_results={this.props.low_calc_results} med_calc_results={this.props.med_calc_results} high_calc_results={this.props.high_calc_results}/>
                                 <Button variant="btn btn-primary btn-lg" type="submit" className="overlay_button" onClick={this.props.handler}>Make Another Calculation</Button>
                           </div>
