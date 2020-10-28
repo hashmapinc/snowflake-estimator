736ae31955402eb58cca450feacb62c3ceb0be8e
Randy Pitcher <randypitcherii@gmail.com>
2020-10-28 16:47:35 -0500

    updated cost calculation

--- src/Components/SnowflakeCost.js
+++ src/Components/SnowflakeCost.js
@@ -12,13 +12,13 @@ class SnowflakeCost extends React.Component {
                 <Container fluid>
                     <Row className="pt-4 pb-4">
                         <Col sm className="text-center">
-                            <h4>{"Low Cost Estimate: $"+(Math.round(this.props.credit_cost*this.props.low_calc_results)).toLocaleString()}</h4>
+                            <h4>{"Low Cost Estimate: $"+(this.props.credit_cost*Math.round(this.props.low_calc_results/12)*12).toLocaleString()}</h4>
                         </Col>
                         <Col sm className="text-center">
-                            <h4>{"Medium Cost Estimate: $"+(Math.round(this.props.credit_cost*this.props.med_calc_results)).toLocaleString()}</h4>
+                            <h4>{"Medium Cost Estimate: $"+(this.props.credit_cost*Math.round(this.props.med_calc_results/12)*12).toLocaleString()}</h4>
                         </Col>
                         <Col sm className="text-center">
-                            <h4>{"High Cost Estimate: $"+(Math.round(this.props.credit_cost*this.props.high_calc_results)).toLocaleString()}</h4>
+                            <h4>{"High Cost Estimate: $"+(this.props.credit_cost*Math.round(this.props.high_calc_results/12)*12).toLocaleString()}</h4>
                         </Col>
                     </Row>
                 </Container>
